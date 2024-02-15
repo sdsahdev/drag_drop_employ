8e60113c3397b4aa8a9e4113489c5156587067b5
sdsahdev <sahdevdomadiya9@gmail.com>
2024-02-15 14:37:48 +0530

    fix

--- Redux/Slice/employSlice.js
+++ Redux/Slice/employSlice.js
@@ -1,28 +1,35 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createSlice } from "@reduxjs/toolkit";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {createSlice} from '@reduxjs/toolkit';
 
 const employSlice = createSlice({
-    name: 'employ',
-    initialState: {
-        employ: []
+  name: 'employ',
+  initialState: {
+    employ: [],
+  },
+  reducers: {
+      addEmploy: (state, action) => {
+        console.log("Current state:", state);
+          state.employ = [...state.employ, action.payload];
+      AsyncStorage.setItem('employ', JSON.stringify(state.employ));
     },
-    reducers: {
-        addEmploy: (state, action) => {
-            state.employ.push(action.payload)
-            
-            AsyncStorage.setItem("employ",JSON.stringify(state.employ) )
-        },
-        updateAllEmploy: (state, action) => {
-            state.employ = []
-            state.employ = action.payload
-        },
-        deleteEmployById: (state, action) => {
-            state.employ = state.employ.filter(employee => employee.id !== action.payload);
-        },
-  
-   
-    }
-})
 
-export const { addEmploy, updateAllEmploy, deleteEmployById } = employSlice.actions;
+    updateAllEmploy: (state, action) => {
+        if (action.payload) {
+            state.employ = [];
+            console.log(action.payload, "====updatedata");
+            state.employ = action.payload;
+        }
+    },
+
+      deleteEmployById: (state, action) => {
+       
+      state.employ = state.employ.filter(
+        employee => employee.id !== action.payload,
+      );
+    },
+  },
+});
+
+export const {addEmploy, updateAllEmploy, deleteEmployById} =
+  employSlice.actions;
 export default employSlice.reducer;
--- src/ShowEmploy.js
+++ src/ShowEmploy.js
@@ -1,38 +1,41 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import DraggableFlatList, { ScaleDecorator } from "react-native-draggable-flatlist";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { updateAllEmploy } from '../Redux/Slice/employSlice';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import DraggableFlatList, {
+  ScaleDecorator,
+} from 'react-native-draggable-flatlist';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {updateAllEmploy} from '../Redux/Slice/employSlice';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-const ShowEmploy = ({ navigation }) => {
-  let datass = useSelector(state => state.employ.employ)
-  dispatch = useDispatch()
-  const renderItem = ({ item, drag, isActive}) => {
-
+const ShowEmploy = ({navigation}) => {
+  let datass = useSelector(state => state.employ.employ);
+  dispatch = useDispatch();
+  const renderItem = ({item, drag, isActive}) => {
     return (
       <ScaleDecorator>
         <TouchableOpacity
           onLongPress={drag}
           disabled={isActive}
-          style={{ backgroundColor: isActive ? 'gray' : 'rgb(0, 152, 247)', borderRadius: 10, margin: 10, alignItems: 'center', padding: 4, }}>
-      
-          <Image source={{ uri: 'https://picsum.photos/200/300' }} style={{ width: 200, height: 150, borderRadius: 10 }} />
+          style={{
+            backgroundColor: isActive ? 'gray' : 'rgb(0, 152, 247)',
+            borderRadius: 10,
+            margin: 10,
+            alignItems: 'center',
+            padding: 4,
+          }}>
+          <Image
+            source={{uri: 'https://picsum.photos/200/300'}}
+            style={{width: 200, height: 150, borderRadius: 10}}
+          />
           <Text style={styles.textshow}>
             {`First Name : ${item.firstname}`}
           </Text>
-          <Text style={styles.textshow}>
-            {`Last Name : ${item.lastName}`}
-          </Text>
-          <Text style={styles.textshow}>
-            {`Email : ${item.email}`}
-          </Text>
+          <Text style={styles.textshow}>{`Last Name : ${item.lastName}`}</Text>
+          <Text style={styles.textshow}>{`Email : ${item.email}`}</Text>
           <Text style={styles.textshow}>
             {`Phone Number : ${item.phonenumber}`}
           </Text>
-          <Text style={styles.textshow}>
-            {`Employe Type : ${item.employ}`}
-          </Text>
+          <Text style={styles.textshow}>{`Employe Type : ${item.employ}`}</Text>
           <Text style={styles.textshow}>
             {`Department Type : ${item.department_select}`}
           </Text>
@@ -40,49 +43,62 @@ const ShowEmploy = ({ navigation }) => {
       </ScaleDecorator>
     );
   };
-useEffect(()=> {
-  getitems()
-
-},[])
-  const onDragEnd = ({ data: newData, from, to }) => {
+  useEffect(() => {
+    getitems();
+  }, []);
+  const onDragEnd = ({data: newData, from, to}) => {
     const newArray = [...datass]; // create a copy of the array
     const temp = newArray[from];
     newArray[from] = newArray[to];
     newArray[to] = temp;
-    dispatch(updateAllEmploy(newArray))
+    dispatch(updateAllEmploy(newArray));
   };
 
   const getitems = async () => {
-    const data = await AsyncStorage.getItem('employ')
-    dispatch(updateAllEmploy(JSON.parse(data)))
-    console.log(JSON.parse(data), "===ghet asuebcd ")
-  }
+    const data = await AsyncStorage.getItem('employ');
+    dispatch(updateAllEmploy(JSON.parse(data)));
+    console.log(JSON.parse(data), '===ghet asuebcd ');
+  };
   return (
     <View>
-      <TouchableOpacity onPress={() => navigation.navigate("AddEmploy")} style={{ backgroundColor: 'gray', padding: 10, alignItems: 'center', margin: 20, borderRadius: 12, }}>
-        <Text style={{ color: '#fff' }}>Add Employ</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('AddEmploy')}
+        style={{
+          backgroundColor: 'gray',
+          padding: 10,
+          alignItems: 'center',
+          margin: 20,
+          borderRadius: 12,
+        }}>
+        <Text style={{color: '#fff'}}>Add Employ</Text>
       </TouchableOpacity>
-      {datass.length === 0 || !datass.every(item => item.id) ? (
-        <Text style={{ color: '#000', textAlign: 'center', textAlignVertical: 'center', justifyContent: 'center', fontSize: 20, }}>No data available</Text>
+      {datass?.length === 0 || !datass?.every(item => item.id) ? (
+        <Text
+          style={{
+            color: '#000',
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            justifyContent: 'center',
+            fontSize: 20,
+          }}>
+          No data available
+        </Text>
       ) : (
         <DraggableFlatList
           scrollEnabled
-          style={{ marginBottom: 150 }}
+          style={{marginBottom: 150}}
           data={datass}
           onDragEnd={onDragEnd}
-          keyExtractor={(item) => item.id}
+          keyExtractor={item => item.id}
           renderItem={renderItem}
         />
-
-
       )}
-
     </View>
-  )
-}
+  );
+};
 
-export default ShowEmploy
+export default ShowEmploy;
 
 const styles = StyleSheet.create({
-  textshow: { color: '#fff', padding: 5, fontSize: 20 }
-})
+  textshow: {color: '#fff', padding: 5, fontSize: 20},
+});
